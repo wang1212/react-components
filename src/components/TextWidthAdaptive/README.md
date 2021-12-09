aaa6b59c07b76c47a3c832279c027f071b7f656a
wangyuan2 <wangyuan2@myhexin.com>
2021-12-09 14:54:06 +0800

    feat: `TextWidthAdaptive` supports multiple lines of text :sparkles:

--- src/components/TextWidthAdaptive/README.md
+++ src/components/TextWidthAdaptive/README.md
@@ -4,7 +4,13 @@ Adaptive adjustment of text width in fixed width container.
 
 > Story: The amount of money is displayed in a fixed-width container, but the initial font size is large, and only a maximum of 100,000 is allowed. Once it reaches this magnitude, the text will exceed the container and affect the overall layout. Therefore, the component is created, and only when the width of the text exceeds the container, the CSS property `transform: scale(n)` is used to scale to fit the width of the container.
 
+> Another story, a fixed-width container needs to display a text that may occupy multiple lines. If the text just occupies one line and exceeds a few characters, do not display it in a new line. Therefore, a limit is provided for the width ratio of the first line of text. When the ratio is less than or equal to this ratio, it will be scaled to allow only one line to be displayed. Otherwise, no special processing will be performed and the display will be automatically wrapped.
+
 ## Usage
+
+### Scene 1
+
+Single line of text.
 
 Source:
 
@@ -21,5 +27,25 @@ To:
   <TextWidthAdaptive>
     $1234567.89
   </TextWidthAdaptive>
+</div>
+```
+
+### Scene 2
+
+Multiline text.
+
+Source:
+
+```
+<div style={{ width: '100px' }}>
+  Some very long text
+</div>
+```
+
+To:
+
+```
+<div style={{ width: '100px' }}>
+  <TextWidthAdaptive text"Some very long text" widthRatioThreshold="1.2" />
 </div>
 ```
--- src/components/TextWidthAdaptive/TextWidthAdaptive.tsx
+++ src/components/TextWidthAdaptive/TextWidthAdaptive.tsx
@@ -1,18 +1,37 @@
 /**
  * Adaptive adjustment of text width in fixed width container.
- * 固定宽度容器中的文字宽度自适应调整
+ *
+ * * Scene 1, the text is only one line, adaptively shrink when it exceeds the width of the container.
+ * ```
+ * <div style={{ width: '100px' }}>
+ *  <TextWidthAdaptive>
+ *    $1234567.89
+ *  </TextWidthAdaptive>
+ * </div>
+ * ```
+ *
+ * * Scene 2, the text may have multiple lines, and the first line will be adaptively reduced when the width of
+ * * the first line does not exceed the specified ratio threshold, otherwise it will not be processed
+ * ```
+ * <div style={{ width: '100px' }}>
+ *  <TextWidthAdaptive text"Some very long text" widthRatioThreshold="1.2" />
+ * </div>
+ * ```
  */
 import React, { useEffect, useRef, useState } from 'react';
-import styles from './index.css';
+import styles from './index.less';
 
 type Props = {
   children: React.ReactNode[];
+  text?: string;
+  widthRatioThreshold?: number;
 };
 
-const TextWidthAdaptive: React.FunctionComponent<Props> = ({ children }) => {
+const TextWidthAdaptive: React.FunctionComponent<Props> = ({ children, text, widthRatioThreshold }) => {
   //
   const $dom = useRef<HTMLElement>();
   const [style, setStyle] = useState({});
+  const [scale, setScale] = useState(true);
 
   useEffect(() => {
     const $parent = $dom.current?.parentElement;
@@ -23,21 +42,30 @@ const TextWidthAdaptive: React.FunctionComponent<Props> = ({ children }) => {
     const parentWidth = $parent.getBoundingClientRect().width;
 
     if (width > parentWidth) {
-      setStyle({ transform: `scale(${parentWidth / width})`, transformOrigin: 'left' });
+      if (!text || width / parentWidth <= widthRatioThreshold) {
+        setStyle({ transform: `scale(${parentWidth / width})`, transformOrigin: 'left' });
+      } else {
+        setScale(false);
+      }
+    } else {
+      setScale(false);
     }
 
     // eslint-disable-next-line consistent-return
     return () => {
       setStyle({});
+      setScale(true);
     };
-  }, [children]);
+  }, [children, text, widthRatioThreshold]);
+
+  if (!scale) return children || text;
 
   // prettier-ignore
   return (
     <span ref={ $dom } className={ styles['m-text'] } style={ style }>
-      { children }
+      { children || text }
     </span>
-  )
+  );
 };
 
 export default TextWidthAdaptive;
